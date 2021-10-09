75a31f1f39496a8bab64a39972f7b785e461ee7b
Per Kristian Svanberg <45123507+lutefisk-development@users.noreply.github.com>
2021-10-09 16:18:12 +0200

    Initial commit

---
+++ getweather.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: 	WP - Get Weather Forecast
+ * Plugin URI: 		https://github.com/lutefisk-development/get-weather-forecast
+ * Description: 	This plugin shows weather forecast
+ * Version: 		0.1
+ * Author: 			Per Kristian Svanberg
+ * Author URI: 		https://github.com/lutefisk-development
+ * License: 		WTFPL
+ * License URI: 	http:// wtfpl.net
+ * Text Domain: 	getweather
+ * Domain Path: 	/languages
+ */
