7802e48fe6d1b2d047032c9805e196a0fed773cf
Per Kristian Svanberg <45123507+lutefisk-development@users.noreply.github.com>
2021-10-10 13:25:48 +0200

    Update class.GetWeather.php
    Working on widget

--- inc/class.GetWeather.php
+++ inc/class.GetWeather.php
@@ -23,12 +23,28 @@ class GetWeather extends WP_Widget {
 	 */
     public function widget($args, $instance) {
         extract($args);
+        $title = apply_filters('widget_title', $instance['title']);
 
         /**
          * Start widget
          *
          */
         echo $before_widget;
+
+        /**
+         * Title
+         *
+         */
+        if (! empty($title)) {
+            echo $before_title . $title . $after_title;
+        }
+
+        $city = ('' !== $instance['city']) ? $instance['city'] : '';
+        ?>
+        <div class="gw__city" data-city="<?php echo $city; ?>">
+            <p><?php _e('Loading ...', 'getweather'); ?></p>
+        </div>
+        <?php
 
         /**
          * close widget
@@ -45,7 +61,39 @@ class GetWeather extends WP_Widget {
 	 * @param array $instance Previously saved values from database.
 	 */
     public function form($instance) {
+        $title = (isset($instance['title']))
+                ? $instance['title']
+                : get_option('gw_default_widget_title', __('Get Weather Forecast', 'getweather'));
 
+        $city = (isset($instance['city']))
+                ? $instance['city']
+                : '';
+
+        ?>
+        <!-- TITLE -->
+        <p>
+            <label for="<?php echo $this->get_field_name('title'); ?>"><?php _e('Title:', 'getweather'); ?></label>
+            <input
+                type="text"
+                id="<?php echo $this->get_field_id('title'); ?>"
+                name="<?php echo $this->get_field_name('title'); ?>"
+                value="<?php echo esc_attr($title); ?>"
+            >
+        </p>
+        <!-- /TITLE -->
+
+        <!-- CITY -->
+        <p>
+            <label for="<?php echo $this->get_field_name('city'); ?>"><?php _e('City:', 'getweather'); ?></label>
+            <input
+                type="text"
+                id="<?php echo $this->get_field_id('city'); ?>"
+                name="<?php echo $this->get_field_name('city'); ?>"
+                value="<?php echo esc_attr($city); ?>"
+            >
+        </p>
+        <!-- /CITY -->
+        <?php
     }
 
     /**
@@ -59,6 +107,14 @@ class GetWeather extends WP_Widget {
 	 * @return array Updated safe values to be saved.
 	 */
     public function update($new_instance, $old_instance) {
+        $instance = [];
+        $instance['title'] = (! empty($new_instance['title']))
+                            ? strip_tags($new_instance['title'])
+                            : '';
+
+        $instance['city'] = (! empty($new_instance['city']))
+                            ? strip_tags($new_instance['city'])
+                            : '';
 
         return $instance;
     }
