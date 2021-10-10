75e99c406d400717753a35b96ab5adfd761fabbd
Per Kristian Svanberg <45123507+lutefisk-development@users.noreply.github.com>
2021-10-10 13:24:50 +0200

    Update main.js
    Started working on ajax call

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,27 @@
-/* HERE BE JAVASCRIPT */
+(function($) {
+    $('.widget_get_weather .gw__city').each(function(i, widget) {
+        let city = $(widget).data('city');
+
+        if ('' !== city) {
+            $.post(ajax_obj.ajax_url, {
+                action: 'get_gw_data',
+                city: city,
+            })
+            .done(function(response) {
+                let output = '';
+                if (response.success) {
+                    let data = response.data;
+
+                    console.log(data);
+
+                    output += `<p>Weather Forecast for: ${city}</p><br>`;
+
+                }
+                $(widget).html(output);
+            })
+            .fail(function(error) {
+                console.error(error)
+            });
+        }
+    });
+})(jQuery);
